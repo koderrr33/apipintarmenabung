f053bd5df1c1566d67b937a33a34dc01b14107dc
Fadhil Afredo <fadhilafredo01@gmail.com>
2026-05-03 10:13:28 +0700

    uhuyy

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,12 @@ export default defineConfig({
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
     },
 });
